eef54e84ffbb499f74345b2d0361b9f11f5ddfd1
aaronjoshua27 <73965654+aaronpangan@users.noreply.github.com>
2022-01-09 17:55:00 +0800

    setup main.py

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+from pydantic.fields import Field
 
 
 app = FastAPI()
 
 
+class Item(BaseModel):
+    hello: Optional[str] = Field("hello", min_length=4)
+    Meow: int
+    d: Optional[bool] = None
+
+
 @app.get("/")
 async def hello_world():
     return {"message": "Hello world"}
+
+
+@app.post("/")
+async def post(body: Item):
+    print(body)
